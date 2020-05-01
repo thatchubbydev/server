2613e5a3850056db28bc785e228ab0fe4c34e24f
thatchubbydev <marazihcarapo@gmail.com>
2020-05-02 00:56:09 +0100

    pushing changes

--- server.js
+++ server.js
@@ -12,8 +12,12 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl:true,
+    // connectionString: process.env.DATABASE_URL,
+    // ssl:true,
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'drago',
+    database: 'postgres'
   },
 });
 
@@ -38,6 +42,6 @@ app.put("/image", (req, res) => {
   image.handleImage(req, res, db);
 });
 
-app.listen(process.env.PORT||3001, () => {
-  console.log(`app is running on port ${process.env.PORT}`);
+app.listen(3001, () => {
+  console.log('app is running on port 3001');
 });
